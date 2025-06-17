c49adaf01c8eb7c74ff5378c8a0e11cf007de867
tiago <tiago.dscouto@hotmail.com>
2025-06-17 16:40:00 -0300

    update menu humburguer

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
+// function scrollToHome(event) {
+//   event.preventDefault();
+//   const home = document.querySelector('#home');
+//   window.scrollTo({
+//     top: home.offsetTop - 300, // compensar o menu fixo
+//     behavior: 'smooth',
+//   });
+// }
+
+// function scrollToInfo(event) {
+//   event.preventDefault();
+//   const info = document.querySelector('#info');
+//   window.scrollTo({
+//     top: info.offsetTop - 80, // compensar o menu fixo
+//     behavior: 'smooth',
+//   });
+// }
+
+function toggleMenu() {
+  const navLinks = document.getElementById('nav-links');
+  if( navLinks.style.display === 'flex' ) {
+    navLinks.style.display = 'none';
+
+  }else {
+    navLinks.style.display = 'flex';
+    navLinks.style.flexDirection = 'column';
+  }
+}
+
 function scrollToHome(event) {
   event.preventDefault();
   const home = document.querySelector('#home');
+  const headerHeight = document.querySelector('nav').offsetHeight;
+
   window.scrollTo({
-    top: home.offsetTop - 300, // compensar o menu fixo
+    top: home.offsetTop - headerHeight,
     behavior: 'smooth',
   });
+
+  fecharMenuSeMobile();
 }
 
 function scrollToInfo(event) {
   event.preventDefault();
   const info = document.querySelector('#info');
+  const headerHeight = document.querySelector('nav').offsetHeight;
+
   window.scrollTo({
-    top: info.offsetTop - 80, // compensar o menu fixo
+    top: info.offsetTop - headerHeight,
     behavior: 'smooth',
   });
+
+  fecharMenuSeMobile();
 }
 
-function toggleMenu() {
+function fecharMenuSeMobile() {
   const navLinks = document.getElementById('nav-links');
-  navLinks.classList.toggle('show');
+  if (window.innerWidth <= 768) {
+    navLinks.style.display = 'none';
+  }
 }
+
 
