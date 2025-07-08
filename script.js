030bf85640288381baf2354a9760c5e1c93214b0
tiago <tiago.dscouto@hotmail.com>
2025-07-08 16:00:52 -0300

    update layout e new push section mission, where are push three values with: mission, vision, values

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function toggleMenu() {
   overlay.classList.toggle('active');
 }
 
-
-
 function fecharMenuSeMobile() {
   const navLinks = document.getElementById('nav-links');
   const overlay = document.getElementById('overlay');
@@ -18,106 +16,7 @@ function fecharMenuSeMobile() {
   }
 }
 
-function scrollToHome(event) {
-  event.preventDefault();
-  const home = document.querySelector('#home');
-  const headerHeight = document.querySelector('nav').offsetHeight;
 
-  window.scrollTo({
-    top: home.offsetTop - headerHeight,
-    behavior: 'smooth',
-  });
-
-  fecharMenuSeMobile();
-}
-
-
-function scrollToContact(event) {
-  event.preventDefault();
-  const contact = document.querySelector('#contato');
-  const headerHeight = document.querySelector('nav').offsetHeight;
-
-  window.scrollTo({
-    top: contact.offsetTop - headerHeight,
-    behavior: 'smooth',
-  });
-
-  fecharMenuSeMobile();
-}
-
-
-
-
-
-function scrollToUnity(event) {
-  event.preventDefault();
-  const unity = document.querySelector('#unity');
-  const headerHeight = document.querySelector('nav').offsetHeight;
-
-  window.scrollTo({
-    top: unity.offsetTop - headerHeight,
-    behavior: 'smooth',
-  });
-
-  fecharMenuSeMobile();
-}
-
-function scrollToInfo(event) {
-  event.preventDefault();
-  const info = document.querySelector('#info');
-  const headerHeight = document.querySelector('nav').offsetHeight;
-
-  window.scrollTo({
-    top: info.offsetTop - headerHeight,
-    behavior: 'smooth',
-  });
-
-  const targetImage = info.querySelector('.article-image img');
-  if (targetImage) {
-    targetImage.classList.remove('scroll-animate');
-    void targetImage.offsetWidth; // força reflow para reiniciar a animação
-    targetImage.classList.add('scroll-animate');
-  }
-
-  fecharMenuSeMobile();
-}
-
-function scrollToHistory(event) {
-  event.preventDefault();
-
-  const history = document.querySelector('#history');
-  const headerHeight = document.querySelector('nav').offsetHeight;
-
-  window.scrollTo({
-    top: history.offsetTop - headerHeight,
-    behavior: 'smooth',
-  });
-
-  // Animação nas imagens
-  const imagens = history.querySelectorAll('.image-history img');
-  imagens.forEach(img => {
-    img.classList.add('scroll-animate');
-    setTimeout(() => {
-      img.classList.remove('scroll-animate');
-    }, 600);
-  });
-
-  fecharMenuSeMobile();
-}
-
-
-function scrollToServices(event) {
-  event.preventDefault();
-  const services = document.querySelector('#services');
-  const headerHeight = document.querySelector('nav').offsetHeight;
-
-  window.scrollTo({
-    top: services.offsetTop - headerHeight,
-    behavior: 'smooth',
-  });
-
-  fecharMenuSeMobile();
-}
 
 
 function openModal(event) {
@@ -243,5 +142,104 @@ function changeImage(clinica, direction) {
   imgElement.src = imageSets[clinica][imageIndexes[clinica]];
 }
 
+function dateNow () {
+  return new Date().getFullYear()
+}
+document.getElementById('footer').innerHTML = `&copy; ${dateNow()} Lavori. Responsável técnico Lawrence de Luca Dias CRM 7010/SC - RQE 12962.`
+// function scrollToHome(event) {
+//   event.preventDefault();
+//   const home = document.querySelector('#home');
+//   const headerHeight = document.querySelector('nav').offsetHeight;
+
+//   window.scrollTo({
+//     top: home.offsetTop - headerHeight,
+//     behavior: 'smooth',
+//   });
+
+//   fecharMenuSeMobile();
+// }
 
 
+// function scrollToContact(event) {
+//   event.preventDefault();
+//   const contact = document.querySelector('#contato');
+//   const headerHeight = document.querySelector('nav').offsetHeight;
+
+//   window.scrollTo({
+//     top: contact.offsetTop - headerHeight,
+//     behavior: 'smooth',
+//   });
+
+//   fecharMenuSeMobile();
+// }
+
+
+// function scrollToUnity(event) {
+//   event.preventDefault();
+//   const unity = document.querySelector('#unity');
+//   const headerHeight = document.querySelector('nav').offsetHeight;
+
+//   window.scrollTo({
+//     top: unity.offsetTop - headerHeight,
+//     behavior: 'smooth',
+//   });
+
+//   fecharMenuSeMobile();
+// }
+
+// function scrollToInfo(event) {
+//   event.preventDefault();
+//   const info = document.querySelector('#info');
+//   const headerHeight = document.querySelector('nav').offsetHeight;
+
+//   window.scrollTo({
+//     top: info.offsetTop - headerHeight,
+//     behavior: 'smooth',
+//   });
+
+//   const targetImage = info.querySelector('.article-image img');
+//   if (targetImage) {
+//     targetImage.classList.remove('scroll-animate');
+//     void targetImage.offsetWidth; // força reflow para reiniciar a animação
+//     targetImage.classList.add('scroll-animate');
+//   }
+
+//   fecharMenuSeMobile();
+// }
+
+// function scrollToHistory(event) {
+//   event.preventDefault();
+
+//   const history = document.querySelector('#history');
+//   const headerHeight = document.querySelector('nav').offsetHeight;
+
+//   window.scrollTo({
+//     top: history.offsetTop - headerHeight,
+//     behavior: 'smooth',
+//   });
+
+//   // Animação nas imagens
+//   const imagens = history.querySelectorAll('.image-history img');
+//   imagens.forEach(img => {
+//     img.classList.add('scroll-animate');
+//     setTimeout(() => {
+//       img.classList.remove('scroll-animate');
+//     }, 600);
+//   });
+
+//   fecharMenuSeMobile();
+// }
+
+
+// function scrollToServices(event) {
+//   event.preventDefault();
+//   const services = document.querySelector('#services');
+//   const headerHeight = document.querySelector('nav').offsetHeight;
+
+//   window.scrollTo({
+//     top: services.offsetTop - headerHeight,
+//     behavior: 'smooth',
+//   });
+
+//   fecharMenuSeMobile();
+// }
